bc04ef7fdc947d9778cb8b2ea65f64eea1e447df
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-11-12 12:44:16 +0500

    Input router is done

--- Front/app/src/App.vue
+++ Front/app/src/App.vue
@@ -3,21 +3,21 @@
 </script>
 
 <template>
- <div class="bg-black  flex justify-center">
+ <div class="  flex justify-center">
   <header class="w-[200px]  ">
     <nav class="flex justify-between  ml-7 w-full   active:text-black">
-      <p class="text-white text-[20px] font-extrabold">
+      <p class="text-black text-[20px] font-extrabold">
         <router-link   to="/input">Input</router-link> 
       </p>
-      <span class="h-2.5 text-[20px] text-white">|</span>
-      <p class="text-white text-[20px]">
+      <span class="h-2.5 text-[20px]text-black ">|</span>
+      <p class="text-black text-[20px]">
         <router-link  to="/dragDrop">Drag_Drop</router-link>
       </p>
     </nav>
   </header>
    </div>  
    <main class="bg-white  h-[92vh] flex justify-center p-10">
-    <section class="bg-red-200  w-[50%] h-[85vh] ">
+    <section class="bg-blue-200 rounded-4xl p-6 w-[50%] h-[85vh] ">
       <router-view></router-view>
     </section>
   </main>
--- Front/app/src/components/Input-uploading/mainInput.vue
+++ Front/app/src/components/Input-uploading/mainInput.vue
@@ -1,3 +1,32 @@
 <template>
-     <h2 class="text-4xl">Input uploading</h2>
+      <main class="flex justify-center">
+          <div class="bg-white   h-[50px] relative rounded-4xl  w-[170px] cursor-pointer active:scale-80  transition-all duration-100 " >
+               <input type="file" @change="handleFile"  class="bg-red-400 w-[170px]   z-10  h-[50px] opacity-0 rounded-4xl absolute  cursor-pointer"/>   
+               
+               <div class="bg-white h-[50px] flex p-2 cursor-pointer active:scale-80  transition-all duration-100 w-[170px] border-2 border-[blue]  rounded-[100px] justify-around">
+                     <Upload/> 
+                     <p class="text-[20px] text-blue-500">Browse files</p>
+               </div>
+          </div>
+
+          <div  v-if="showContent" class="absolute top-[200px] ">
+               <h1><span class="text-xl">Name:</span>{{ inputValue.name }}</h1>
+               <h1><span class="text-xl">Size:</span> {{ (inputValue.size / 1024).toFixed(2) }} KB</h1>
+          </div>
+      </main>
+      
 </template>
+<script setup>
+import { ref } from 'vue';
+import Upload from '../icons/upload.vue';
+
+const inputValue = ref(null)
+const showContent = ref(false)
+
+const handleFile = (e)=>{
+     
+     inputValue.value=e.target.files[0]
+     showContent.value = true
+     console.log(inputValue.value)
+}
+</script>
